9da8f1bdd6cfd6860550cc8de842f342f1049c8c
amraniyoussef <youssefamrani9@gmail.com>
2014-10-21 00:59:07 +0200

    Dev2 Commit 2

--- Test_Final_Java/src/Page1.java
+++ Test_Final_Java/src/Page1.java
@@ -7,6 +7,7 @@ public class Page1 {
 	public static void main(String[] args) {
 		System.out.println(" Uef : Bonjour ACSIS ");
 		System.out.println(" Dev2 : Salut ucef ");
+		System.out.println(" Dev2 : cava ?? ");
 	}
 
 }
