352116dffe7db62444f429f0911c73bbd99daaac
Hamza <Hamza@10.42.29.94>
2014-10-22 17:00:15 +0200

    ham

--- Test_Final_Java/src/Page1.java
+++ Test_Final_Java/src/Page1.java
@@ -5,12 +5,7 @@ public class Page1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(" Uef : Bonjour ACSIS ");
-		System.out.println(" Dev2 : Salut ucef ");
-
-		System.out.println(" Dev2 : cava ?? ");
-        System.out.println("jena");
-		System.out.println("Hamzaa");
+		System.out.println("Voila faites pull");
 		
 
 	}
