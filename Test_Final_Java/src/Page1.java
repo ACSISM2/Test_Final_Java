caa9ff25f5a7aa007b4ad33de493f42ef4aa9266
anis <anis@10.42.4.5>
2014-10-22 17:29:18 +0200

    COMMIT ANIS

--- Test_Final_Java/src/Page1.java
+++ Test_Final_Java/src/Page1.java
@@ -7,7 +7,7 @@ public class Page1 {
 	public static void main(String[] args) {
 		System.out.println("anis hmoriyall");
 		System.out.println("Voila faites pull");
-
+		System.out.println("2");
 		
 
 	}
